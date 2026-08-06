08d881a0ba5cf39c36606fd30a60bb1607e5e7e4
Rafael Pampavilca <rafopm@gmail.com>
2026-08-05 22:08:21 -0500

    update id evento con id invitación

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // Cambia solo aquí cuando despliegues nuevo exec
-const API_BASE = "https://script.google.com/macros/s/AKfycbyLNVde6PhFkZdxKQoAMCkHUi_5ZQcz5G6LsNCiqXNdOJixnyLvvVGeOmCg5qrI4YXWeA/exec";
+const API_BASE = "https://script.google.com/macros/s/AKfycbzUkdwRwsyQwBqEPeM4lJK5Z74QXLfXzdDL4YmleBtIca_J_4S-gOH_LLm_hFRMAHUEFA/exec";
