88631cb58cbaee4fdc62810eb1fbfee7be0f32a5
Rafael Pampavilca <rafopm@gmail.com>
2026-08-05 22:23:21 -0500

    validación de correo

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // Cambia solo aquí cuando despliegues nuevo exec
-const API_BASE = "https://script.google.com/macros/s/AKfycbzUkdwRwsyQwBqEPeM4lJK5Z74QXLfXzdDL4YmleBtIca_J_4S-gOH_LLm_hFRMAHUEFA/exec";
+const API_BASE = "https://script.google.com/macros/s/AKfycbxmHLTT3jOl4syByki4lbyrU5RX4pdnLHBWEnjUfpaTEh1Vr1EXJx8kOaon_rycoiTR1g/exec";
