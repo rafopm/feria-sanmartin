1d944b66f337880f938d841e1fb05906fda32dc3
Rafael Pampavilca <rafopm@gmail.com>
2026-08-05 21:28:45 -0500

    add config con exec appscript

---
+++ config.js
@@ -0,0 +1,2 @@
+// Cambia solo aquí cuando despliegues nuevo exec
+const API_BASE = "https://script.google.com/macros/s/AKfycbyLNVde6PhFkZdxKQoAMCkHUi_5ZQcz5G6LsNCiqXNdOJixnyLvvVGeOmCg5qrI4YXWeA/exec";
