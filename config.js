725a428c5a9ac54a85881820ac4a7ca724d0f58b
Rafael Pampavilca <rafopm@gmail.com>
2026-08-05 22:39:43 -0500

    update verificar correo

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // Cambia solo aquí cuando despliegues nuevo exec
-const API_BASE = "https://script.google.com/macros/s/AKfycbxmHLTT3jOl4syByki4lbyrU5RX4pdnLHBWEnjUfpaTEh1Vr1EXJx8kOaon_rycoiTR1g/exec";
+const API_BASE = "https://script.google.com/macros/s/AKfycbzPB6wWGfJrF_BFfhUyed9Whj65NtlBatDUvDZ5sVWUmZZO3NATpc2gH1dGjxo8CaOL/exec";
